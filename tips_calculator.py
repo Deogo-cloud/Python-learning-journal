c12b2b1c115a37fb1d6897baae0c13b08ff2a6f5
Deogo-cloud <lordyexp@gmail.com>
2026-08-01 22:24:12 +0630

    Delete tips_calculator.py

--- tips_calculator.py
+++
@@ -1,7 +0,0 @@
-print("--Tips calculator--")
-meals_price=float(input("Meals price:"))
-tips_percentage=int(input("Tips percentage:"))
-
-tips_amount=meals_price*(tips_percentage/100)
-total_bills=tips_amount+meals_price
-print("Total bills:",total_bills)
