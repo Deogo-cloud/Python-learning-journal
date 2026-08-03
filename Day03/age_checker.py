2512f9c34d9d133ef1c72c0ecabee8f2ed31706f
Deogo-cloud <lordyexp@gmail.com>
2026-08-03 22:42:04 +0630

    Fix age checker logic and improve output messages

--- Day03/age_checker.py
+++ Day03/age_checker.py
@@ -1,16 +1,17 @@
 age = int(input("Enter the age:"))
 
-if age < 12:
-    print("You are a child")
-elif age < 0:
+
+if age < 0:
     print("Invalid age!")
+elif age < 12:
+    print("You are a child")
 elif age < 20:
-    print("You are a tennager")
+    print("You are a teenager")
 elif age < 40:
     print("You are an adult")
 elif age < 60:
     print("You are middle age")
 elif age < 140:
-    print("You are  old age")
+    print("You are old")
 else:
-    print("You are  immortal!")
+    print("You are immortal!")
