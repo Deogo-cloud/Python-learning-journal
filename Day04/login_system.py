6bcd83fc03a6dc7ae5f5090c93a44b8697f25174
Deogo-cloud <lordyexp@gmail.com>
2026-08-04 16:40:38 +0630

    Implement basic login system with age eligibility

---
+++ Day04/login_system.py
@@ -0,0 +1,13 @@
+username = "deogo"
+password = "deogo123"
+login_username = input("Enter username:")
+login_password = input("Enter password:")
+if login_username == username and login_password == password:
+    print("Login sucesssful!")
+else:
+    print("Login failed!")
+age = int(input("enter your age:"))
+if age >= 18 and age <= 30:
+    print("eligible age")
+else:
+    print("not eligible")
