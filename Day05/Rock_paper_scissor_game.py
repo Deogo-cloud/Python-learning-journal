0b49d7b5c3ceb5f593f7e19f74aa072b3552dc34
Deogo-cloud <lordyexp@gmail.com>
2026-08-06 16:02:07 +0630

    Implement Rock-Paper-Scissors game

---
+++ Day05/Rock_paper_scissor_game.py
@@ -0,0 +1,41 @@
+import random
+
+rock = 1
+scissor = 2
+paper = 3
+
+while True:
+    ai = random.randint(1, 3)
+    player = int(input("0.exit 1.rock 2.scissor 3.paper: "))
+
+    if player == 0:
+        print("Game Over")
+        break
+    elif player not in [1, 2, 3]:
+        print("Please choose valid number")
+        continue
+
+    # AI chooses Rock (1)
+    if ai == 1 and player == 1:
+        print("AI: Rock | You: Rock -> Draw")
+    elif ai == 1 and player == 2:
+        print("AI: Rock | You: Scissors -> AI Wins!")
+    elif ai == 1 and player == 3:
+        print("AI: Rock | You: Paper -> You Win!")
+
+    # AI chooses Scissors (2)
+    elif ai == 2 and player == 1:
+        print("AI: Scissors | You: Rock -> You Win!")
+    elif ai == 2 and player == 2:
+        print("AI: Scissors | You: Scissors -> Draw")
+    elif ai == 2 and player == 3:
+        print("AI: Scissors | You: Paper -> AI Wins!")
+
+    # AI chooses Paper (3)
+    elif ai == 3 and player == 1:
+        print("AI: Paper | You: Rock -> AI Wins!")
+    elif ai == 3 and player == 2:
+        print("AI: Paper | You: Scissors -> You Win!")
+    elif ai == 3 and player == 3:
+        print("AI: Paper | You: Paper -> Draw")
+      
